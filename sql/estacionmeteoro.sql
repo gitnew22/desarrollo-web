88be597b0726995b4a0f33344ca14cc3b265f8c7
gitnew22 <asturblok@gmail.com>
2020-02-05 18:03:53 +0100

    2

---
+++ sql/estacionmeteoro.sql
@@ -0,0 +1,24 @@
+﻿CREATE DATABASE estacionMeteo;
+USE estacionMeteo;
+
+CREATE TABLE estacion(
+id mediumint UNSIGNED,
+  latitud varchar(14),
+  longitud varchar(15),
+  altitud mediumint,
+  PRIMARY KEY(id)
+);
+
+CREATE TABLE muestra(
+idEstacion mediumint UNSIGNED,
+  fecha date,
+  temMinima tinyint,
+  temMaxima tinyint,
+  precipitaciones smallint UNSIGNED,
+  humedadMin tinyint UNSIGNED,
+  humedMax tinyint UNSIGNED,
+  velocidadVientoMin smallint UNSIGNED,
+  velocidadVientoMax smallint UNSIGNED,
+  PRIMARY KEY (idEstacion,fecha),
+  FOREIGN KEY (idEstacion) REFERENCES estacion(id) 
+);
---
+++ sql/libreria.sql
@@ -0,0 +1,22 @@
+﻿CREATE DATABASE IF NOT EXISTS libreria;
+USE libreria;
+
+CREATE TABLE libro(
+claveLibro varchar(20),
+titulo varchar(30),
+  idioma varchar(20),
+  formato varchar(10),
+  categria varchar(10),
+  claveEditorial varchar(10),
+  PRIMARY KEY(claveLibro)
+);
+
+CREATE TABLE tema(
+claveTema varchar(10),
+  nombre varchar(20),
+  PRIMARY KEY(claveTema)
+);
+
+CREATE TABLE autor(
+
+);
