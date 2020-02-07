d6a3b5ca55558264d5fbd0fcf6ec4ffb619b3bea
gitnew22 <asturblok@gmail.com>
2020-02-07 19:56:07 +0100

    2

---
+++ sql/consultasempledepart.sql
@@ -0,0 +1,29 @@
+﻿USE empresa;
+
+  /* 1 mostrar todos los campos y todos los registros de la tabla empleado*/
+    SELECT * FROM emple e;
+  /* 2 mostrar todos los campos y todos los registros de la tabla departamento*/ 
+    SELECT * FROM depart d;
+  /* 3 mostrar el apellido y oficio de cada empleado */
+    SELECT e.apellido FROM emple e;
+  /* 4 mostrar la localizacion y numero de cada empleado*/
+    SELECT e.emp_no, e.dir FROM emple e;
+  /* 5 mostrar el numero, nombre y localizacion de cada departamento*/
+     SELECT d.dept_no, d.dnombre FROM depart d;
+  /* 6 indicar el numero de empleados que hay */
+      SELECT COUNT(*) FROM emple e; 
+  /* 7 datos de los empleados ordenados por apellido de forma ascendente */
+      SELECT * FROM emple e ORDER BY e.apellido;
+  /* 8 datos de los empleados ordenados por apellido de forma descendente*/
+      SELECT * FROM emple e ORDER BY e.apellido DESC;
+  /* 9 indicar el numero de departamentos que hay*/
+      SELECT COUNT(*) FROM depart d;
+  /* 10 indicar el numero de empleados mas el numero de departamentos */
+      SELECT
+      (SELECT COUNT(*) FROM emple e)
+        +
+      (SELECT COUNT(*) FROM depart d);
+  /* 11 datos de los empleados ordenados por numero de departamento descendentemente*/
+      SELECT * FROM emple e ORDER BY e.dept_no DESC;
+  /* 12 datos de los empleados ordenados por numero de departamento ascendentemente*/
+      SELECT * FROM emple e ORDER BY e.dept_no;
--- sql/empledepart.sql
+++ sql/empledepart.sql
@@ -1,5 +1,7 @@
 ﻿CREATE DATABASE empresa;
 USE empresa;
+
+
 CREATE TABLE depart(
 dept_no int(11),
   dnombre varchar(30),
@@ -25,20 +27,20 @@ INSERT INTO depart (dept_no, dnombre, loc)
 (30, 'ventas', 'barcelona'),
 (40, 'produccion', 'bilbao');
 
-INSERT INTO emple (emp_no, apellido, oficio, dir, fecha_alt, salario, comision, dep_no)
-  VALUES  (7369, 'sanchez', 'empleado', 7902,(17/12/1990), 1040, NULL, 20),
-          (7499, 'arroyo', 'vendedor', 7698,(20/02/1990), 1500, 390, 30),
-          (7521, 'sala', 'vendedor', 7698,(22/02/1991), 1625, 650, 30),
-          (7566, 'jimenez', 'director', 7839,(02/04/1991), 2900, NULL ,20),
-          (7654, 'martin', 'vendedor', 7698,(29/09/1991), 1600, 1020, 30),
-          (7698, 'negro', 'director', 7839,(01/05/1991), 3005, NULL, 30),
-          (7782, 'cerezo', 'director', 7839,(09/06/1991), 2885, NULL, 10),
-          (7788, 'gil', 'analista', 7566,(09/11/1991), 3000,NULL,20),
-          (7839, 'rey', 'presidente', NULL,(17/11/1991), 4100, NULL, 10),
-          (7844, 'tovar', 'vendedor', 7698,(08/09/1991), 1350, 0, 30),
-          (7876, 'alonso', 'empleado', 7788,(23/09/1991), 1430,NULL , 20),
-          (7900, 'jimeno', 'empleado', 7698,(03/12/1991), 1335, NULL, 30),
-          (7902, 'fernandez', 'analista', 7566,(03/12/1991), 3000, NULL ,20),
-          (7934, 'muñoz', 'empleado', 7782,(23/01/1992), 1690, NULL ,10);
+INSERT INTO emple (emp_no, apellido, oficio, dir, fecha_alt, salario, comision, dept_no)
+  VALUES  (7369, 'sanchez', 'empleado', 7902,'1770/12/10', 1040, NULL, 20),
+          (7499, 'arroyo', 'vendedor', 7698,'2044/02/90', 1500, 390, 30),
+          (7521, 'sala', 'vendedor', 7698,'2772/02/01', 1625, 650, 30),
+          (7566, 'jimenez', 'director', 7839,'2202/04/19', 2900, NULL ,20),
+          (7654, 'martin', 'vendedor', 7698,'4429/09/19', 1600, 1020, 30),
+          (7698, 'negro', 'director', 7839,'1441/05/19', 3005, NULL, 30),
+          (7782, 'cerezo', 'director', 7839,'4409/06/11', 2885, NULL, 10),
+          (7788, 'gil', 'analista', 7566,'1551/19/30',2345,NULL,20),
+          (7839, 'rey', 'presidente', NULL,'5517/11/11', 4100, NULL, 10),
+          (7844, 'tovar', 'vendedor', 7698,'6608/09/19', 1350, 0, 30),
+          (7876, 'alonso', 'empleado', 7788,'2773/09/19', 1430,NULL , 20),
+          (7900, 'jimeno', 'empleado', 7698,'5503/12/19', 1335, NULL, 30),
+          (7902, 'fernandez', 'analista', 7566,'1993/12/19',3000, NULL ,20),
+          (7934, 'muñoz', 'empleado', 7782,'2993/01/19',1690, NULL ,10);
 
 
